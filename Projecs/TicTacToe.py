1198aa4696c36119be0e4ee7169caed0178f1dff
Vinicius Bueno <62633022+VinicBueno@users.noreply.github.com>
2023-03-17 02:56:17 -0300

    Update TicTacToe.py

--- Projecs/TicTacToe.py
+++ Projecs/TicTacToe.py
@@ -1,5 +1,4 @@
-import pygame
-import sys
+import pygame, sys
 
 # initialize Pygame
 pygame.init()
@@ -8,6 +7,8 @@ pygame.init()
 WINDOW_SIZE = (400, 400)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 LINE_WIDTH = 6
 
 # create the game window
@@ -31,10 +32,10 @@ def draw_board():
     for row in range(3):
         for col in range(3):
             if board[row][col] == 'X':
-                pygame.draw.line(screen, BLACK, (col * 133 + 20, row * 133 + 20), (col * 133 + 113, row * 133 + 113), LINE_WIDTH)
-                pygame.draw.line(screen, BLACK, (col * 133 + 20, row * 133 + 113), (col * 133 + 113, row * 133 + 20), LINE_WIDTH)
+                pygame.draw.line(screen, RED, (col * 133 + 20, row * 133 + 20), (col * 133 + 113, row * 133 + 113), LINE_WIDTH)
+                pygame.draw.line(screen, RED, (col * 133 + 20, row * 133 + 113), (col * 133 + 113, row * 133 + 20), LINE_WIDTH)
             elif board[row][col] == 'O':
-                pygame.draw.circle(screen, BLACK, (col * 133 + 66, row * 133 + 66), 47, LINE_WIDTH)
+                pygame.draw.circle(screen, BLUE, (col * 133 + 66, row * 133 + 66), 47, LINE_WIDTH)
 
 def check_winner():
     # check rows
@@ -68,11 +69,10 @@ def player_move(row, col, piece):
     else:
         return False
 
-# set up the game loop
-turn = 'X'
+# st up the game loop
 game_over = False
-
-while not game_over:
+turn = 'X'
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -80,11 +80,9 @@ while not game_over:
         elif event.type == pygame.MOUSEBUTTONDOWN and not game_over:
             # get the coordinates of the mouse click
             mouseX, mouseY = pygame.mouse.get_pos()
-
             # determine which row and column the mouse click corresponds to
             row = mouseY // 133
             col = mouseX // 133
-
             # place the player's piece on the board
             if player_move(row, col, turn):
                 if check_winner() is not None:
@@ -95,14 +93,22 @@ while not game_over:
                         turn = 'O'
                     else:
                         turn = 'X'
-
+        elif event.type == pygame.MOUSEBUTTONDOWN and game_over:
+            # redefine as variáveis do jogo
+            game_over = False
+            turn = 'O'
+            board = [[None, None, None], [None, None, None], [None, None, None]]
+            
+            # redesenha o tabuleiro vazio
+            draw_board()
+            
     # draw the game board
     screen.fill(WHITE)
     draw_board()
-
+    
     # check for a winner
     if check_winner() is not None:
-        game_over = False
+        game_over = True
         if check_winner() == 'tie':
             message = 'Tie game!'
         else:
@@ -110,9 +116,9 @@ while not game_over:
         font = pygame.font.Font(None, 48)
         text = font.render(message, True, BLACK)
         screen.blit(text, (WINDOW_SIZE[0] // 2 - text.get_width() // 2, WINDOW_SIZE[1] // 2 - text.get_height() // 2))
-
+    
     # update the game window
     pygame.display.update()
-
+    
 # exit Pygame
 pygame.quit()
